39f6ebadcd2849e00706ff0f74a5ea36aa02ea62
Joaquin <joaquinmengarelli@gmail.com>
2020-09-26 23:30:01 -0300

    models primer intento

--- Verduleria/models.py
+++ Verduleria/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
 # Create your models here.
+
+class dia(models.Model):
+    fechadelacompra = models.DateTimeField()
+
+
+class Productos(models.Model):
+    tipodeproducto = models.CharField(max_length=15)
+    producto = models.CharField(max_length=15)
+    cantidadXkilo = models.CharField(max_length=15)
+    precioXkilo = models.CharField(max_length=15)
+
+class Cliente(models.Model):
+    nombre = models.CharField(max_length=15)
+    apellido = models.CharField(max_length=15)
+    dni = models.CharField(max_length=15)
+    domicilio = models.CharField(max_length=15)
+
+class Factura(models.Model):
+    dia = models.CharField(max_length=15)
+    Productos = models.CharField(max_length=15)
+    Cliente = models.CharField(max_length=15)
+    

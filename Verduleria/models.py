ad16c6523b37db30927bab4508fcf0efb90e3054
Joaquin <joaquinmengarelli@gmail.com>
2020-09-27 16:59:10 -0300

    borre migrations

--- Verduleria/models.py
+++ Verduleria/models.py
@@ -1,21 +1,24 @@
 from django.db import models
 
 # Create your models here.
-class Factura(models.Model):
-    Productos = models.ForeignKey('Productos', on_delete=models.CASCADE)
-    Cliente = models.ForeignKey('CLiente', on_delete=models.CASCADE)
 
 class dia(models.Model):
     fechadelacompra = models.DateTimeField()
 
-class Productos(models.Model):
+class Producto(models.Model):
     tipodeproducto = models.CharField(max_length=15)
     producto = models.CharField(max_length=15)
     cantidadXkilo = models.CharField(max_length=15)
     precioXkilo = models.CharField(max_length=15)
+
 
 class Cliente(models.Model):
     nombre = models.CharField(max_length=15)
     apellido = models.CharField(max_length=15)
     dni = models.CharField(max_length=15)
     domicilio = models.CharField(max_length=25)
+
+
+class Factura(models.Model):
+    Cliente = models.ForeignKey('Cliente', on_delete=models.CASCADE)
+    productos = models.ManyToManyField(Producto)
